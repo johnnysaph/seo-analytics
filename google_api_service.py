746218cee9743869c5c1544fcafd4fa68d4e0e98
Ivan Safonov <42542612+johnnysaph@users.noreply.github.com>
2026-07-11 17:20:20 +0300

    Created google_api_service.py
        Get a service that communicates to a Google API.
        Args:
            api_name: The name of the api to connect to.
            api_version: The api version to connect to.
            scopes: A list auth scopes to authorize for the application.
        Returns:
            A service that is connected to the specified API.

---
+++ google_api_service.py
@@ -0,0 +1,35 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import sys
+import os
+
+# path to json creds
+GOOGLE_KEY_PATH = os.getenv("GOOGLE_API_KEY_FILE_PATH")
+
+def get_credentials(google_api_key_path):
+    if not google_api_key_path:
+        raise ValueError("GOOGLE_API_KEY_FILE_PATH is not set")
+        
+    credentials = service_account.Credentials.from_service_account_file(
+        google_api_key_path)
+    return credentials
+
+def get_service(api_name, api_version, scopes):
+    """
+    Get a service that communicates to a Google API.
+    Args:
+        api_name: The name of the api to connect to.
+        api_version: The api version to connect to.
+        scopes: A list auth scopes to authorize for the application.
+    Returns:
+        A service that is connected to the specified API.
+    """
+    
+    try:
+        credentials = get_credentials(GOOGLE_KEY_PATH)
+        service = build(api_name, api_version, credentials=credentials, cache_discovery=False)
+        return service
+    except:
+        logging.error('Google Authentication Error occurred.')
+        logging.error(ex)
+        sys.exit(1)
